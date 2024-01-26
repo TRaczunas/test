2ca22d0a3bc9545e5bdf98dc006107dc5456086c
TRaczunas <155486651+TRaczunas@users.noreply.github.com>
2024-01-26 19:31:06 +0100

    Create zad3_test.py

---
+++ zad3_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestMyModule(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(isprime(13), True)
+        self.assertEqual(isprime(19), True)
+        self.assertEqual(isprime(27), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
+  
